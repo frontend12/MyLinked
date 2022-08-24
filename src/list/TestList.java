90704c9e28ebe1ce092d3abec123658b1aab8597
Stas Shtykov <stasstykov@gmail.com>
2022-08-24 16:30:19 +0300

    Initial commit

---
+++ src/list/TestList.java
@@ -0,0 +1,11 @@
+package list;
+
+public class TestList {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(22);
+        System.out.println(myLinkedList);
+    }
+}
